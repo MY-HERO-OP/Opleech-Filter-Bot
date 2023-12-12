3d6e2cae3931ca7dea71f6d7cd00463f569f1a31
MY-HERO-OP <129320317+MY-HERO-OP@users.noreply.github.com>
2023-12-12 22:00:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,7 +15,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '1703392'))
 API_HASH = environ.get('API_HASH', '96089a3407f2892fd06aea683cbfb73c0')
-BOT_TOKEN = environ.get('BOT_TOKEN', "5931364008:AAGsovwZScQxeaJZByrxiZscW1JsO4SsrG30")
+BOT_TOKEN = environ.get('BOT_TOKEN', "5931364008:AAyGsovwZScQxeaJZByrxiZscW1JsO4SsrG30")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
